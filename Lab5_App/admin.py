b52d6ce1e006368e37e182cbd173f00c5d36fbaa
Denizche <Denizche1204@gmail.com>
2018-01-15 13:21:59 +0300

    added small useful things

--- Lab5_App/admin.py
+++ Lab5_App/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from loginsys import models
-
 # Register your models here.
 
-admin.site.register(models.Service)
-admin.site.register(models.UserService)
+
+class ServiceClass(admin.ModelAdmin):
+    fields = (('id', 'title'), 'price', 'picture')
+    list_filter = ('price',)
+    list_display = ('id', 'title', 'price', 'picture')
+    search_fields = ('title', 'id')
+    list_per_page = 10
+
+class UserServiceClass(admin.ModelAdmin):
+    fields = (('id', 'service'), 'user', 'description', 'desc_picture')
+    list_filter = ('id', 'user')
+    list_display = ('id', 'service', 'user', 'description', 'desc_picture')
+    search_fields = ('user', 'service')
+    list_per_page = 10
+
+
+admin.site.register(models.Service, ServiceClass)
+admin.site.register(models.UserService, UserServiceClass)
